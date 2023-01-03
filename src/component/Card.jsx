58487b0e91b228e1e0444c12f3e0f0c7715202db
Brayan <smithrivas.dev@gmail.com>
2023-01-03 12:34:53 -0500

    Se agregó color dinamico a textos y botón

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -6,34 +6,25 @@ const Card = ({data})=>{
     const{name, email, phone} = data[random]
     const randomNumber = ()=>{
         setRandom(Math.floor(Math.random() * data.length))
+    }    
+    const colors = ["#FDB137", "#785964", "#6D6875", "#B5838D", "#E5989B", "#7E9680", "#C73866", "#FFB4A2", "#79616F", "#EAB595" ]
+    document.body.style = `background-color: ${colors[random]}; color: ${colors[random]}`;        
+    const divStyle = {
+        color: `${colors[random]}`
     }
-    const colors = [
-        "#ff7e5fc7",
-        "#77ff5f",
-        "#ff605f",
-        "#f15fff",
-        "#55d0ff80",
-        "#4b54fff7",
-        "#ffe835",
-        "#35e9ffcc",
-        "#a506b4b5",
-        "#29d500b8"
-    ]
-    document.body.style = `background-color: ${colors[random]}`;        
     return(
         <div className="card"> 
             <div className="dataCard">
                 <h1>{`${name.title} ${name.first} ${name.last}`}</h1>            
                 <img src={`${data[random].picture.large}`} alt="" />
-                <p><i class='bx bxs-envelope'></i> {email}</p>
-                <p><i class='bx bxs-phone' ></i> {phone}</p>
-                <p><i class='bx bxs-map' ></i> {`${data[random].location.country} ${data[random].location.state} ${data[random].location.city}`}</p>
+                <p><i className='bx bxs-envelope'></i> {email}</p>
+                <p><i className='bx bxs-phone' ></i> {phone}</p>
+                <p><i className='bx bxs-map' ></i> {`${data[random].location.country} ${data[random].location.state} ${data[random].location.city}`}</p>
             </div>
             <div className="button">
-                <button onClick={()=>randomNumber()}><i className='bx bx-transfer' ></i></button>         
+                <button onClick={()=>randomNumber()}><i style={divStyle} className='bx bx-transfer' ></i></button>         
             </div>
-        </div>
-        
+        </div>        
     )
 }
 
